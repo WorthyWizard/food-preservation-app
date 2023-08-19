3d03deffb48e1204aa501f72a197b60cfe837c4d
WorthyWizard <amilogames@gmail.com>
2023-08-19 18:27:37 +0300

    Add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,48 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['simple-import-sort'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^react', '^react-native', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['@/features/*/*', '@/lib/*/*'],
+      },
+    ],
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ExportDefaultDeclaration',
+        message: 'Default exports are not allowed',
+      },
+    ],
+  },
 };
